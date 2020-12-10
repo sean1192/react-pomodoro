6b4d6cf9221e58c2a806dc75a2ac54cddf83c59b
Sean <sean.tbear@gmail.com>
2020-12-09 21:04:18 -0500

    Basic hello world react directory

---
+++ src/components/Pomodoro.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+class Pomodoro extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // include initial state values here
+        };
+
+        // bind functions here
+        // ie. this.handleClick = this.handleClick.bind(this);
+    }
+
+    render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+};
+
+export default Pomodoro
